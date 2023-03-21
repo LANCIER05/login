e9dd9836e74ac87d1e38d45cb1b38df2cd130e84
LANCIER05 <ulanturgunbaiev22@gmail.com>
2023-03-21 22:38:48 +0600

    initial

---
+++ script.js
@@ -0,0 +1,15 @@
+function a (){
+    const btn = document.querySelector(".password__btn")
+    const input = document.querySelector(".password__input")
+
+    btn.addEventListener("click", () => {
+        btn.classList.toggle('active')
+
+        if (input.getAttribute('type') === 'password'){
+            input.setAttribute('type', 'text')
+        } else {
+            input.setAttribute('type', 'password')
+        }
+    })
+}
+a()
